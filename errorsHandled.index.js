0958f2f0799a8d27b9e986cb16c9e92e33a091c1
Juho Kangas <juhokn@gmail.com>
2023-09-27 16:01:02 +0300

    remove the body-parser also from the errorhandling version

--- errorsHandled.index.js
+++ errorsHandled.index.js
@@ -1,11 +1,10 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const fs = require('fs')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Load the initial data from the JSON file
 let todoData = JSON.parse(fs.readFileSync('database.json', 'utf-8'))
